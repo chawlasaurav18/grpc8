8dbc5bc60fb7ddc3ef690c8960f1dfd434d6a3fa
Saurav Chawla <chawlasaurav18@gmail.com>
2025-03-03 14:51:45 +0530

    saved

---
+++ src/main/java/com/vinsguru/Main.java
@@ -0,0 +1,11 @@
+package com.vinsguru;
+import com.vinsguru.models.Person;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("saurav");
+        System.out.println("hii");
+
+
+    }
+}
